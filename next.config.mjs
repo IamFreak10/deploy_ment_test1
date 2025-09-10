ace6f70d469f2186ed71e4db6d92228909cee157
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2025-09-11 02:24:36 +0600

    Update: Disable Turbopack and update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,18 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.ibb.co.com', // allow this domain
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com', // ✅ Cloudinary
+        protocol: "https",
+        hostname: "i.ibb.co.com", // allow this domain
+      },{
+        protocol: "https",
+        hostname: "i.postimg.cc",
+      },{
+        protocol: "https",
+        hostname: "res.cloudinary.com", // ✅ Cloudinary
       },
     ],
   },
-  experimental: {
-    turbo: { enabled: false }, 
-  },
+  turbopack:false,
 };
 
 export default nextConfig;
